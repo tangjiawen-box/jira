521c63d26f4bf95744d10d4ad0904106353bb30b
1185101290@qq.com <1185101290@qq.com>
2023-02-07 00:35:32 +0800

    test

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -4,6 +4,10 @@ export const useUrlQueryParam = (keys: string[]) =>{
     const [URLSearchParams, SetURLSearchParams] = useSearchParams()
     // useSearchParams组件会返回一个URLSearchParams，这个是浏览器的对象
     // 不能直接调用，
+    
+    
+    
+    
     console.log(URLSearchParams.get('name'));
     
 }
